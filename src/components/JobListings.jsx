368f646131556731e125083edfd6ff843218a79f
13Aashishgit <er.13aashishgupta@gmail.com>
2024-07-30 01:15:03 +0530

    Changes some UI part

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -29,11 +29,11 @@ const JobListings = ({ isHome = false }) => {
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          {isHome ? "Recent Jobs" : "Browse Jobs"}
+          {isHome ? "Recent jobs" : "Browse jobs"}
         </h2>
         
           {loading ? (
-            <h2>Loading</h2>
+            <h2>Loading...</h2>
           ) : (
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
               {jobs.map((job) => (
